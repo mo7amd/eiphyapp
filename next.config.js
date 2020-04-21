0a064d04f8d9d8e7049e05ea2bc0553977f63058
mohamed khaled <mo7amd.khaled@gmail.com>
2020-04-21 04:48:07 +0200

    add the upload page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withSass = require('@zeit/next-sass');
 const withPurgeCss = require('next-purgecss');
 
-module.exports = withSass(withPurgeCss());
+module.exports = withSass(withPurgeCss({
+  purgeCssEnabled: ({ dev, isServer }) => (!dev && !isServer),
+  env: {
+    MAX_FILE_SIZE: 5,
+  },
+}));
