0be258379630734d645e23c9ab9b4474c234243e
ahmedengu <ahmedengu@gmail.com>
2020-04-21 03:06:18 +0200

    search

--- pages/search/[param].js
+++ pages/search/[param].js
@@ -1,26 +1,68 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import Router from 'next/router';
+import Grid from '../../components/grid';
+import Layout from '../../components/layout';
+import { db } from '../../lib/firebase';
 
-function Search() {
-  const router = useRouter();
-  const { param } = router.query;
-  return (
-    <>
-      <Head>
-        <title>
-          search -
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgs: [],
+      type: 'meme',
+      param: 'merquant',
+      search: ''
+    };
+  }
+
+  componentDidMount() {
+    const { type } = this.state;
+
+    db.collection('posts')
+      .where('type', '==', type)
+      .get()
+      .then((querySnapshot) => {
+        const imgs = [];
+        querySnapshot.forEach((doc) => imgs.push(doc.data()));
+        this.setState({ imgs });
+      });
+  }
+
+  render() {
+    const { param, imgs, search } = this.state;
+    return (
+      <Layout>
+        <Head>
+          <title>
+            search -
+            {param}
+          </title>
+        </Head>
+        <h2>
+          hello
+          {' - '}
           {param}
-        </title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="ta7ouna" />
-      </Head>
-      <h2>
-        hello
-        {' - '}
-        {param}
-      </h2>
-    </>
-  );
+        </h2>
+        <input type="search" value={search} onChange={(e) => {
+          this.setState({ search: e.target.value });
+        }}/>
+        <button
+          type="button"
+          onClick={() => {
+            Router.push(`/search/${search}`);
+          }}
+        >
+          search
+        </button>
+        <Grid imgs={imgs}/>
+
+      </Layout>
+    );
+  }
 }
+
 
 export default Search;
