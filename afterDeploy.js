593bcb6d78159c7a8e5fe856668bf033c4b184b4
ahmedengu <ahmedengu@gmail.com>
2020-04-24 21:54:21 +0200

    CF purge working

--- afterDeploy.js
+++ afterDeploy.js
@@ -2,10 +2,10 @@ const request = require('request');
 
 const options = {
   method: 'DELETE',
-  url: 'https://api.cloudflare.com/client/v4/zones/XXXXXXXXXXXXXXXXXXX/purge_cache',
+  url: 'https://api.cloudflare.com/client/v4/zones/f23fd60c8e10242d98d7b083e88ff25d/purge_cache',
   headers: {
     'content-type': 'application/json',
-    authorization: 'Bearer XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authorization: 'Bearer crPBYFn4QpBYc6EyYUtsnx5Gm8U5joFzoV1US19d',
   },
   body: { purge_everything: true },
   json: true,
