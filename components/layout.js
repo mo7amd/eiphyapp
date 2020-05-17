cda3df821ab55c054cc70b9c9c171c2888155228
ahmedengu <ahmedengu@gmail.com>
2020-05-17 14:37:55 +0200

    loading splash

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import Footer from './footer';
 import Header from './header';
 
-const Layout = ({ children }) => (
-  <>
-    <div id="layout">
-      <Header />
-      <main className="main-layout">
-        {children}
-      </main>
-      <Footer />
-    </div>
-  </>
-);
+const Layout = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const onLoad = () => setIsLoading(false);
+  const onBeforeUnload = () => setIsLoading(true);
+
+  if (process.browser) {
+    useLayoutEffect(() => {
+      if (document.readyState === 'complete') {
+        setTimeout(onLoad, 3000);
+      }
+      window.addEventListener('beforeunload', onBeforeUnload);
+      window.addEventListener('load', onLoad);
+      return () => {
+        window.removeEventListener('load', onLoad);
+        window.removeEventListener('beforeunload', onBeforeUnload);
+      };
+    }, []);
+  }
+
+  return (
+    <>
+      <div id="divLoading" className={isLoading ? 'show' : ''} />
+      <div id="layout">
+        <Header />
+        <main className="main-layout">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
