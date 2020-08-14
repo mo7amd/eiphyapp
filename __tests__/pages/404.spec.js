8b265a1245009a1d6ddc6dc34ed6b73e86590093
ahmedengu <ahmedengu@gmail.com>
2020-08-14 23:47:34 +0200

    fix build

--- __tests__/pages/404.spec.js
+++ __tests__/pages/404.spec.js
@@ -1,8 +1,8 @@
 import { mount } from 'enzyme';
 import { axe } from 'jest-axe';
-import Custom404 from './404';
+import Custom404 from '../../pages/404';
 
-jest.mock('../components/header', () => () => <header>Header</header>);
+jest.mock('../../components/header', () => () => <header>Header</header>);
 
 function setup(props = {}) {
   return mount(<Custom404 msg="this is an error" {...props} />);
